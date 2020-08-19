ffc077d9323e92d2682f3b246be9e90bc291b7e9
Dan Rusu <dan.rusu@rms.com>
2020-08-20 02:30:30 +0300

    added static folders & data update endpoint (PUT)

--- app.js
+++ app.js
@@ -2,33 +2,114 @@
 
 const express = require('express'); 
 const app = express();
+const { readFile, writeFile } = require('fs');
+const { promisify } = require('util');
+const [ read, write ] = [ readFile, writeFile ].map(promisify);
 
 var path = require('path');
 const bodyParser = require('body-parser');
+const { group } = require('console');
 
 app.use(express.json());
-app.use(express.static('/'));
+app.use(express.static('public'));
+app.use('/data', express.static('data'));
+
+const DATA_TYPES = ['import', 'accumulation'];
+const prettify = json => JSON.stringify(json, null, 2);
 
 const serveFileFromRoot = (res, relativePath) => 
     res.sendFile(path.join(`${__dirname}/${relativePath}`));
 
 const serveHome = (_, res) => serveFileFromRoot(res, 'index.html');
-const serveImportData = (_, res) => serveFileFromRoot(res, 'import.json');
-const serveAccumulationData = (_, res) => serveFileFromRoot(res, 'accumulation.json');
-const serveFavicon = (_, res) => serveFileFromRoot(res, 'favicon.ico');
-const serveMainCss = (_, res) => serveFileFromRoot(res, 'main.css');
-const serveMainJs = (_, res) => serveFileFromRoot(res, 'main.js');
+
+const NO_OF_LOCATIONS = {
+    '1K': 1,
+    '100K': 100,
+    '250K': 250,
+    '500K': 500,
+    '1M': 1000,
+    '4M': 4000,
+    '5M': 5000,
+};
+
+const formatWorkflowData = workflowData => {
+    const {date, noOfLocations, duration} = workflowData;
+    return {
+        name: `_${date.replace(/-/g, '_')}`,
+        noOfLocations: NO_OF_LOCATIONS[noOfLocations],
+        duration: duration.ms
+    };
+}
+
+const saveWorkflowData = async (workflowData, dataType) => {
+
+
+   // validate workflowData.type vs. dataType
+
+   if (! Object.keys(NO_OF_LOCATIONS).includes(workflowData.noOfLocations)){
+       console.log(`Wrong nr. of locations: ${workflowData.noOfLocations}. Not saved!`);
+       return;
+   }
+
+   const dataFilePath = path.join(`${__dirname}/data/${dataType}.json`);
+
+    const formatedWorkflowData = formatWorkflowData(workflowData);
+    console.log(`formatedWorkflowData: ${prettify(formatedWorkflowData)}`);
+    const { name, noOfLocations, duration } = formatedWorkflowData;
+
+    const currentData = JSON.parse(await read(dataFilePath));
+    const groupNameIndex = currentData.findIndex(group => group.name === name);
+
+    if (groupNameIndex > -1){
+        const values = currentData[groupNameIndex].values;
+        const noOfLocationsIndex = values.findIndex(value => value.noOfLocations === noOfLocations);
+        
+        if (noOfLocationsIndex > -1){
+            values[noOfLocationsIndex].duration = duration;
+        }
+        else {
+            values.push({ noOfLocations, duration });            
+        }
+        values.sort((v1, v2) => v1.noOfLocations < v2.noOfLocations ? -1 : 1);
+    }
+    else{
+        currentData.push({ 
+            name,
+            values: [ { noOfLocations, duration } ]
+        });        
+    }
+    console.log(prettify(currentData));
+
+    await write(dataFilePath, prettify(currentData));
+};
+
+const updateData = (req, res) => {
+    const dataType = req.params.dataType;
+    
+    if (! DATA_TYPES.includes(dataType)){
+        res.status(404).send(`Wrong dataType! Only ${DATA_TYPES} are accepted`);
+        return;
+    }
+
+    if (req.headers['content-type'] !== 'application/json'){ 
+        res.status(406).send('Only JSON is supported!');
+        return;
+    }
+    
+    const workflowData = req.body;
+    console.log(`Received ${dataType} workflow data: ${prettify(workflowData)}`);
+
+    saveWorkflowData(workflowData, dataType);
+
+    res.status(202).send(`${dataType} upload success`);
+};
 
 // routes
 app.get('/', serveHome);
-app.get('/import', serveImportData);
-app.get('/accumulation', serveAccumulationData);
-app.get('/favicon.ico', serveFavicon);
-app.get('/main.css', serveMainCss);
-app.get('/main.js', serveMainJs);
+app.put('/data/:dataType', updateData);
 
 const port = process.env.PORT || 1111;
 
 app.listen(port, () =>
-    console.log(`visual-d3 server listening at http://localhost:${port}/`)
+    console.log(`Express server listening at http://localhost:${port}   `)
 );
--- public/main.js
+++ public/main.js
@@ -1,3 +1,20 @@
+const addXAxisLegend = (svg, legendText, width, height) => svg.append("text")
+  .attr("class", "x label")
+  .attr("text-anchor", "end")
+  .attr("x", width)          
+  .attr("y", height - 10)
+  .text(legendText)
+  .style('fill', 'blue');
+
+const addYAxisLegend = (svg, legendText) => svg.append("text")
+  .attr("class", "y label")
+  .attr("text-anchor", "end")
+  .attr("y", 6)
+  .attr("dy", ".75em")
+  .attr("transform", "rotate(-90)")
+  .text(legendText)
+  .style('fill', 'blue');
+
 const displayGraph = data => {
       
     d3.selectAll("svg").remove();
@@ -9,7 +26,7 @@ const displayGraph = data => {
       bottom: 50, 
       left: 100
     },
-    width = 800 - margin.left - margin.right,
+    width = 1200 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
@@ -116,38 +133,24 @@ const displayGraph = data => {
             d3.selectAll("." + d.name).transition().style("opacity", currentOpacity == 1 ? 0:1)
           });
 
-      //     
-      svg.append("text")
-        .attr("class", "x label")
-        .attr("text-anchor", "end")
-        .attr("x", width)          
-        .attr("y", height - 10)
-        .text("Portfolio size [no. of locations]")
-        .style('fill', 'blue');;
+      addXAxisLegend(svg, 'Portfolio size [no. of locations * 1000]', width, height);      
+      addYAxisLegend(svg, 'Duration [ms]');
+      
+};
 
-      svg.append("text")
-        .attr("class", "y label")
-        .attr("text-anchor", "end")
-        .attr("y", 6)
-        .attr("dy", ".75em")
-        .attr("transform", "rotate(-90)")
-        .text("Duration [ms]")
-        .style('fill', 'blue');
-      };
-
-  const displayGraphFromData = async dataEndpointName => {   
-    const response = await fetch(dataEndpointName);
-    const data = await response.json();
-    console.log(data);
+const displayGraphFromData = async dataName => {   
+  const response = await fetch(`/data/${dataName}.json`);
+  const data = await response.json();
+  console.log(data);
     
-    displayGraph(data);
-  };
+  displayGraph(data);
+};
 
-  const resultTypeOnChange = () => {
-    const resultTypeButtons = [].slice.call(document.querySelectorAll('[name="resultType"]'))
-      .forEach(resultTypeButton =>
-          document.getElementById(`${resultTypeButton.id}TypeTxt`).className = resultTypeButton.checked ?
-            'resultTypeSelected'
-            : ''          
-      );
-  }
+const resultTypeOnChange = () => {
+  const resultTypeButtons = [].slice.call(document.querySelectorAll('[name="resultType"]'))
+    .forEach(resultTypeButton =>
+        document.getElementById(`${resultTypeButton.id}TypeTxt`).className = resultTypeButton.checked ?
+          'resultTypeSelected'
+          : ''          
+    );
+}
